60859ec2fd74a7fd14477ee93a1c3863a77df2a5
orian <pawelszczur@gmail.com>
2015-11-05 09:44:27 +0100

    change StdEncoding to UrlEncoding

--- ids/ids.go
+++ ids/ids.go
@@ -9,14 +9,27 @@ import (
 func Encode(id int64) string {
 	b := make([]byte, 20)
 	n := binary.PutVarint(b, id)
-	return base64.StdEncoding.EncodeToString(b[:n])
+	return base64.URLEncoding.EncodeToString(b[:n])
 }
 
 func Decode(str string) (int64, error) {
-	data, err := base64.StdEncoding.DecodeString(str)
+	data, err := base64.URLEncoding.DecodeString(str)
 	if err != nil {
 		return 0, err
 	}
 	return binary.ReadVarint(bytes.NewReader(data))
 }
 
+func EncodeOld(id int64) string {
+	b := make([]byte, 20)
+	n := binary.PutVarint(b, id)
+	return base64.StdEncoding.EncodeToString(b[:n])
+}
+
+func DecodeOld(str string) (int64, error) {
+	data, err := base64.StdEncoding.DecodeString(str)
+	if err != nil {
+		return 0, err
+	}
+	return binary.ReadVarint(bytes.NewReader(data))
+}
