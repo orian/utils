60f3583b93e70967ceb63ef959e899dfa615551e
Paweł Szczur <ps@datainq.com>
2018-07-27 11:06:55 +0200

    Fix unsafe concurrent access to source.

--- others/others.go
+++ others/others.go
@@ -4,25 +4,38 @@ package others
 import (
 	"math/rand"
 	"time"
+	"sync"
 )
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const (
+	letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 	letterIdxBits = 6                    // 6 bits to represent a letter index
 	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
 	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
 )
 
-var src = rand.NewSource(time.Now().UnixNano())
+var (
+	src = rand.NewSource(time.Now().UnixNano())
+	m sync.Mutex
+)
+
+
+func int63() int64 {
+	m.Lock()
+	x := src.Int63()
+	m.Unlock()
+	return x
+}
 
 // RandString produce a random string
 // comes from: http://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
 func RandString(n int) string {
 	b := make([]byte, n)
 	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+	for i, cache, remain := n-1, int63(), letterIdxMax; i >= 0; {
 		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
+			cache, remain = int63(), letterIdxMax
 		}
 		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
 			b[i] = letterBytes[idx]
@@ -35,5 +48,5 @@ func RandString(n int) string {
 	return string(b)
 }
 
-// TODO, it would be even coler to just byte64 encode a random byte sequence.
+// TODO it would be even cooler to just byte64 encode a random byte sequence.
 
